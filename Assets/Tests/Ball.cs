754eebcb9acfbc9e74cece050d28659d2975944c
THE-ANIMUS\PERSONAL <skafreak1969@gmail.com>
2018-08-25 01:30:59 -0500

    Tests

---
+++ Assets/Tests/Ball.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+
+public class Ball : MonoBehaviour {
+    Rigidbody rigid;
+    bool start;
+    float startTime;
+	// Use this for initialization
+	void Start () {
+        rigid = gameObject.GetComponent<Rigidbody>();
+        start = false;
+        startTime = 2f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!start)
+        {
+            startTime -= Time.deltaTime;
+            if (startTime<=0)
+            {
+                rigid.AddForce(new Vector3(-0.05f, 0, -0.2f), ForceMode.Impulse);
+                start = true;
+            }
+        }
+        if (start)
+        {
+            if (rigid.velocity.z < 1 )
+            {
+                rigid.AddForce(new Vector3(0, 0, -1f));
+            }
+            if (rigid.velocity.x < 1)
+            {
+                rigid.AddForce(new Vector3(-1f, 0, 0));
+            }
+        }
+
+        rigid.velocity = new Vector3(rigid.velocity.x, 0, rigid.velocity.z);
+    }
+
+    public void RestartBall()
+    {
+        startTime = 2f;
+        start = false;
+        transform.position = new Vector3(0, 0.01f, 0);
+        rigid.velocity = new Vector3(0, 0, 0);
+    }
+
+    [UnityTest]
+    public IEnumerator Test_Ball_YVelocity()
+    {
+        var go = new GameObject();
+        go.AddComponent<Ball>();
+        go.AddComponent<Rigidbody>();
+
+
+        yield return new WaitForSeconds(5f);
+
+        Assert.AreEqual(go.GetComponent<Rigidbody>().velocity.y, 0);
+    }
+}
---
+++ Assets/Tests/PlayerAi.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+
+public class PlayerAi : MonoBehaviour {
+    bool side;
+	// Use this for initialization
+	void Start () {
+        side = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (side)
+        {
+            transform.Translate(transform.right * Time.deltaTime * 6f);
+            if (transform.position.x > 1.6f)
+            {
+                side = false;
+            }
+        }
+        else
+        {
+            transform.Translate(-transform.right * Time.deltaTime * 6f);
+            if (transform.position.x < -1.6f)
+            {
+                side = true;
+            }
+        }
+        
+	}
+
+    [UnityTest]
+    public IEnumerator Test_AI_Position()
+    {
+        var go = new GameObject();
+        go.AddComponent<PlayerAi>();
+
+        
+        yield return new WaitForSeconds(1f);
+
+        Assert.Less(go.transform.position.x, 1.6f);
+    }
+}
