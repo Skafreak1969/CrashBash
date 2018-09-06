df880f9457b7e635d6a6bf76e0067f1e076d008d
DESKTOP-892PSIB\EQUIPO <juanlol94@hotmail.com>
2018-09-06 16:59:23 -0500

    Merge the 2 projects

---
+++ Assets/ChangePlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangePlayer : MonoBehaviour {
+    [SerializeField] GameObject player;
+	// Use this for initialization
+	void Start () {
+        CambiarScript(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void CambiarScript(bool valor)
+    {
+        if (valor)
+        {
+            player.GetComponent<PlayerDrag>().enabled = true;
+            player.GetComponent<PlayerButtons>().enabled = false;
+        }
+        else
+        {
+            player.GetComponent<PlayerDrag>().enabled = false;
+            player.GetComponent<PlayerButtons>().enabled = true;
+        }
+    }
+}
---
+++ Assets/PlayerAi.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAi : MonoBehaviour {
+    bool side;
+	// Use this for initialization
+	void Start () {
+        side = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (side)
+        {
+            transform.Translate(transform.right * Time.deltaTime * 6f);
+            if (transform.position.x > 3)
+            {
+                side = false;
+            }
+        }
+        else
+        {
+            transform.Translate(-transform.right * Time.deltaTime * 6f);
+            if (transform.position.x < -3)
+            {
+                side = true;
+            }
+        }
+        
+	}
+}
